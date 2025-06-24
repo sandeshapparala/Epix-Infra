44c99f211039ba95195f654bae218b46ea72f934
sandeshapparala <sandeshapparala@gmail.com>
2025-06-24 09:11:36 +0530

    added team members

--- src/components/sections/TeamSection.tsx
+++ src/components/sections/TeamSection.tsx
@@ -3,7 +3,6 @@
 
 
 import {TeamMemberCard} from "@/components/team/team-card";
-import {ContactCard} from "@/components/team/contact-card";
 
 const teamMembers = [
     {
@@ -17,13 +16,23 @@ const teamMembers = [
         role: "9032069013",
         image: "/Images/jprakash.jpg",
         initials: "MC"
-    }
-];
+    },
+    {
+        name: "Nandu A",
+        role: "",
+        image: "/Images/nandu.jpg",
+        initials: "MC"
+    },
+    {
+        name: "Sudinesh Reddy",
+        role: "",
+        image: "/Images/sreddy.jpg",
+        initials: "MC"
+    },
 
-const contactInfo = {
-    email: "hello@epixinfra.com",
-    phone: "+91 7386440344"
-};
+
+
+];
 
 export function TeamSection() {
     return (
@@ -33,14 +42,14 @@ export function TeamSection() {
                     Our Team
                 </h2>
 
-                <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+                <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
                     {teamMembers.map((member, index) => (
                         <TeamMemberCard key={index} {...member} />
                     ))}
-                    <ContactCard
-                        email={contactInfo.email}
-                        phone={contactInfo.phone}
-                    />
+                    {/*<ContactCard*/}
+                    {/*    email={contactInfo.email}*/}
+                    {/*    phone={contactInfo.phone}*/}
+                    {/*/>*/}
                 </div>
             </div>
         </section>
